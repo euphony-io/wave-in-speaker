7dc88fe6d59090c1682d0391759e06456e2a2585
Sooyeon Jo <supop1009@gmail.com>
2021-09-28 00:42:07 +0900

    Add VisitRecordActivity

---
+++ app/src/main/java/com/example/check_in_speaker/VisitRecordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.check_in_speaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VisitRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visit_record)
+    }
+}
