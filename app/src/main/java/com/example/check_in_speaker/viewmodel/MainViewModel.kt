9fe51681357cd6a068018872dff092f17f20bfe1
Sooyeon Jo <supop1009@gmail.com>
2021-10-02 13:59:19 +0900

    Add isValidSafeNumber() to verify the safe number

--- app/src/main/java/com/example/check_in_speaker/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/check_in_speaker/viewmodel/MainViewModel.kt
@@ -10,12 +10,23 @@ class MainViewModel : ViewModel() {
     private var _isClickCheckInButton = MutableLiveData(true)
     val isClickCheckInButton : LiveData<Boolean>
         get() = _isClickCheckInButton
+
+    private val _isValidSafeNumber = MutableLiveData<Boolean>()
+    val isValidSafeNumber: LiveData<Boolean>
+      get() = _isValidSafeNumber
+
     private val mTxManager: EuTxManager by lazy {
         EuTxManager()
     }
 
     fun onClickCheckInButton() {
         _isClickCheckInButton.value = _isClickCheckInButton.value != true
+    }
+
+    private val safeNumberFormat = Regex("^[가-힣][0-9]{2}[가-힣][0-9]{2}\$")
+
+    fun isValidSafeNumber(safeNumber: String){
+        _isValidSafeNumber.value = safeNumber.matches(safeNumberFormat)
     }
 
     fun focusStatusIsFailed() {
