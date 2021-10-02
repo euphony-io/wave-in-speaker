fb332cecdfa78f2977a1a5238348ea6e4ed48440
Sooyeon Jo <supop1009@gmail.com>
2021-10-02 14:01:23 +0900

    Update safe number dialog in MainActivity

--- app/src/main/java/com/example/check_in_speaker/MainActivity.kt
+++ app/src/main/java/com/example/check_in_speaker/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.check_in_speaker
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.media.AudioAttributes
 import android.media.AudioFocusRequest
 import android.media.AudioManager
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.Window
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.check_in_speaker.databinding.ActivityMainBinding
+import com.example.check_in_speaker.databinding.DialogSafeNumberBinding
+import com.example.check_in_speaker.util.PreferencesUtil
 import com.example.check_in_speaker.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity(){
@@ -19,7 +27,10 @@ class MainActivity : AppCompatActivity(){
     private lateinit var mainViewModel: MainViewModel
     private lateinit var audioFocusRequest: AudioFocusRequest
     private lateinit var audioManager: AudioManager
+    private lateinit var dialog: Dialog
+    private lateinit var prefs: PreferencesUtil
 
+    private var dialogBinding: DialogSafeNumberBinding? = null
     private var isClicked : Boolean = false
     private var initVolume = 0
     private var safeNumber = "hello"
@@ -46,8 +57,27 @@ class MainActivity : AppCompatActivity(){
             isClicked = it
         }
 
+        mainViewModel.isValidSafeNumber.observe(this){
+            if(it){
+                prefs.setString("safeNumber", dialogBinding!!.dialogEtSafeNumber.text.toString())
+                binding.tvMainSafeNumber.text = prefs.getString("safeNumber", "")
+                dialog.dismiss()
+            }else{
+                Toast.makeText(this, "유효하지 않는 안심번호입니다. 다시 확인해주세요.", Toast.LENGTH_LONG).show()
+            }
+        }
+
         binding.btnMainCheckin.setOnClickListener {
             clickCheckIn()
+        }
+        
+        prefs = PreferencesUtil(applicationContext)
+
+        if(prefs.getString("safeNumber","") == ""){
+            initDialog()
+            dialog.show()
+        }else{
+            binding.tvMainSafeNumber.text = prefs.getString("safeNumber", "")
         }
     }
 
@@ -158,6 +188,22 @@ class MainActivity : AppCompatActivity(){
             setInitVolume()
         } else {
             mainViewModel.focusStatusIsFailed()
+        }
+    }
+
+    private fun initDialog(){
+        dialogBinding = DialogSafeNumberBinding.inflate(LayoutInflater.from(baseContext))
+
+        dialog = Dialog(this)
+        dialog.apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            setCancelable(false)
+            setContentView(dialogBinding!!.root)
+        }
+
+        dialogBinding!!.dialogTvOk.setOnClickListener {
+            mainViewModel.isValidSafeNumber(dialogBinding!!.dialogEtSafeNumber.text.toString())
         }
     }
 }
